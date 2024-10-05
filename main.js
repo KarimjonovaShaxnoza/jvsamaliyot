db47fca95b59d426ed823430fb8c6cae71875e01
KarimjonovaShaxnoza <shaxnozakarimjonova100@gmail.com>
2024-10-05 17:35:48 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let body = document.querySelector('body');
+let text = document.querySelector('#text');
+
+let bgColor = prompt('enter body backgroundColor');
+let textColor = prompt('enter text color');
+
+body.style.backgroundColor = bgColor;
+text.style.color = textColor;
